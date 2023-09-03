d97b5c9723a4d97b847515fd88e67df819e8937d
kalevishnu <113785595+kalevishnu@users.noreply.github.com>
2023-09-03 16:19:07 +0530

    new Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,9 @@ public class Sum{
     public static void main(String[] args) {
         int a =10;
         int b =20;
-        System.out.println( "sum of ab"a+b);
+        int sum =a+b;
+        System.out.println( "sum of ab" + sum);
+        
 
     }
 }
